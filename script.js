552611f36717509225ccc173431f9cecf2d92703
Vishal Attri <attrivishal95@hotmail.com>
2021-01-25 02:39:18 +0530

    Api Url Fixed

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ async function getQuote() {
   const apiUrl =
     "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
   try {
-    const response = await fetch(apiUrl);
+    const response = await fetch(proxyUrl + apiUrl);
     const data = await response.json();
     // If Author is blank, then 'Unknown'
     if (data.quoteAuthor === "") {
